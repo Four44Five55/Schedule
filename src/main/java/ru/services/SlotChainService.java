96a6a67349f2bd796e6f692b7c6effd76a2ec0b0
Four44Five55 <Four4Five55@yandex.ru>
2025-05-14 20:31:24 +0300

    -feat внесены изменения в SlotChainService

--- src/main/java/ru/services/SlotChainService.java
+++ src/main/java/ru/services/SlotChainService.java
@@ -2,19 +2,33 @@ package ru.services;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.sql.Update;
 import org.springframework.stereotype.Service;
+import ru.entity.logicSchema.CurriculumSlot;
 import ru.entity.logicSchema.SlotChain;
 import ru.repositories.SlotChainRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class SlotChainService {
     private final SlotChainRepository repository;
+    private final CurriculumSlotService slotService;
 
-    public SlotChain create(SlotChain slotChain) {
-        return repository.save(slotChain);
+    public void createChain(Integer slotAId, Integer slotBId) {
+        if (slotAId.equals(slotBId)) {
+            throw new IllegalArgumentException("Cannot chain slot to itself");
+        }
+        if (repository.existsBySlotAIdAndSlotBId(slotAId, slotBId)) {
+            throw new IllegalStateException("Chain already exists");
+        }
+
+        CurriculumSlot slotA = slotService.getById(slotAId);
+        CurriculumSlot slotB = slotService.getById(slotBId);
+
+        repository.save(new SlotChain(slotA, slotB));
     }
 
     public SlotChain getByID(Integer id) {
@@ -33,5 +47,26 @@ public class SlotChainService {
     public SlotChain save(SlotChain slotChain) {
         return repository.save(slotChain);
 
+    }
+
+    public void removeChain(Integer slotAId, Integer slotBId) {
+        repository.deleteBySlotAIdAndSlotBId(slotAId, slotBId);
+    }
+
+    public boolean isChained(Integer slotAId, Integer slotBId) {
+        return repository.existsBySlotAIdAndSlotBId(slotAId, slotBId) ||
+                repository.existsBySlotAIdAndSlotBId(slotBId, slotAId);
+    }
+
+    public List<Integer> getLinkedSlotIds(Integer slotId) {
+        List<Integer> linkedIds = new ArrayList<>();
+
+        repository.findBySlotAId(slotId)
+                .forEach(sc -> linkedIds.add(sc.getSlotB().getId()));
+
+        repository.findBySlotBId(slotId)
+                .forEach(sc -> linkedIds.add(sc.getSlotA().getId()));
+
+        return linkedIds;
     }
 }
